45d4c064aab9eed353bca9f17b205143812b79e0
user <user@DESKTOP-NAOE6C8>
2022-09-23 07:04:21 +0200

    finished app

---
+++ MolnkursMVC/Data/MolnkursMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MolnkursMVC.Models;
+
+namespace MolnkursMVC.Data
+{
+    public class MolnkursMVCContext : DbContext
+    {
+        public MolnkursMVCContext (DbContextOptions<MolnkursMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MolnkursMVC.Models.Movie> Movie { get; set; } = default!;
+    }
+}
--- MolnkursMVC/Program.cs
+++ MolnkursMVC/Program.cs
@@ -1,9 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MolnkursMVC.Models;
+using MolnkursMVC.Data;
+using MolnkursMVC.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<MolnkursMVCContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MolnkursMVCContext")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
